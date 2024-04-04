ea95181d436065716e288a2c4b661fb7c1f1c8ce
alaa <alaaabobakr2@gmail.com>
2024-04-04 23:07:05 +0200

    +feature_splash +splash_screen

--- lib/main.dart
+++ lib/main.dart
@@ -16,10 +16,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter'),
-      //ergtrhrbtrsxhzd
-      //sthzsehzesjdzhrsj
-    );
+      home: const MyHomePage(title: 'Flutter hoooooooo'),
+
   }
 }
 
